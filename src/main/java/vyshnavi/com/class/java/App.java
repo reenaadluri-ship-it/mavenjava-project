dcf59ddfdc0b4b06763c0513899a29b7ce650883
reenaadluri-ship-it <reenaadluri@gmail.com>
2026-08-07 14:01:53 +0530

    Initial commit

---
+++ src/main/java/vyshnavi/com/class/java/App.java
@@ -0,0 +1,10 @@
+package vyshnavi.com.class.java;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
